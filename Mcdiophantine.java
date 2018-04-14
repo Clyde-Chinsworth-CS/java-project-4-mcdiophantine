88ec0c4271530df9e991778fe3b4ee60bf5d8093
Alex Yang <a.yang2@share.epsb.ca>
2018-04-14 17:31:42 +0000

    Added loop for twenties

--- Mcdiophantine.java
+++ Mcdiophantine.java
@@ -11,10 +11,13 @@ public class Mcdiophantine
 	int nuggets = kbReader.nextInt(); //program will calculate values for highest possible number less than or equal to user input
 	for (int sum = 1; sum <= nuggets; sum++)
 	{
-		for (int i = 0; i * 6 <= sum; i++) //loops for every value of 6 * i untill it is greater or equal to amount of nuggets 
+		for (int i = 0; i * 6 <= sum; i++) //loops for every value of 6 * i until it is greater or equal to amount of nuggets 
 		{
-			for (int j = 0; i * 6 + j * 9 <= sum; j++) //loops for every value of i * 6 + j * 9, i remains constant throughtout all trials
+			for (int j = 0; i * 6 + j * 9 <= sum; j++) //loops for every value of (i * 6) + (j * 9), i remains constant throughout all trials
 			{
+				for (int k = 0; i * 6 + j * 9 + k * 20 <= sum; k++) //loops for every value of (i * 6) + (j * 9) + (k * 9), i and j remains constant throughout all trials
+				{
+				}
 			}
 		} 
 	
