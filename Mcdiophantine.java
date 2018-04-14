a0a1b75ca50d5005c1d097cf5ac6522a17e4b6bb
Alex Yang <a.yang2@share.epsb.ca>
2018-04-14 16:38:35 +0000

    Declared counter variables

--- Mcdiophantine.java
+++ Mcdiophantine.java
@@ -8,8 +8,14 @@ public class Mcdiophantine
 	{
 	Scanner kbReader = new Scanner(System.in);
 	System.out.print("Enter the amount of McNuggets the program will run up to: ");
-	double nuggets = kbReader.nextDouble();
-
+	double nuggets = kbReader.nextDouble(); //program will calculate values for highest possible number less than or equal to user input
+	for (double i = 1; i <= nuggets)
+	{
+		int twenties = 0; //counts number of twenties that can fit
+		int nines = 0; // counts number of nines that can fit
+		int sixes = 0; // counts number of sixes that can fit 
+		
+	}
 	
 	}
 }
