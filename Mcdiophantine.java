b9578eda9ae84334b1d080e1230c59b13e027cfc
Alex Yang <a.yang2@share.epsb.ca>
2018-04-14 16:32:00 +0000

    Added user input for program length

--- Mcdiophantine.java
+++ Mcdiophantine.java
@@ -1,12 +1,15 @@
 //Mcdiophantine Project
 //14/04/2018
-import java.io*.;
+import java.io.*;
 import java.util.*;
 public class Mcdiophantine
 {
 	public static void main(String args[])
 	{
-	
+	Scanner kbReader = new Scanner(System.in);
+	System.out.print("Enter the amount of McNuggets the program will run up to: ");
+	double nuggets = kbReader.nextDouble();
 
+	
 	}
 }
